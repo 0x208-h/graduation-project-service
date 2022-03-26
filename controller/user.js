fa7e75888c1672986a23fc3f20d567554ce233e0
0x208-h <huangchenhui0812@163.com>
2022-03-26 23:18:35 +0800

    feat: 修改登录接口的回传数据

--- controller/user.js
+++ controller/user.js
@@ -14,9 +14,9 @@ exports.get = async function (req, res, next) {
 
 exports.login = async function (req, res, next) {
   try {
-    console.log(req.body, "req");
     // 数据验证
     // 生成token
+    const user = req.body;
     const token = await jwt.sign(
       {
         userId: req.body.user_id,
@@ -27,7 +27,37 @@ exports.login = async function (req, res, next) {
       }
     );
 
-    res.status(200).send({ ...req.body, token });
+    const sql = "select * from users where username = ? and password = ?";
+    const sql1 = "select * from users where username = ?";
+    const sqlArr1 = [user.user_id];
+    const sqlArr = [user.user_id, user.password];
+    const ret = await db(sql, sqlArr);
+    const ret1 = await db(sql1, sqlArr1);
+    console.log(ret);
+    if (ret1.length > 0) {
+      if (ret.length > 0) {
+        delete user.password;
+        return res.status(200).send({
+          status: 200,
+          statusText: "success",
+          ...user,
+          token,
+          message: "登录成功",
+        });
+      } else {
+        res
+          .status(200)
+          .send({
+            status: 200,
+            statusText: "fail",
+            message: "用户名或者密码错误",
+          });
+      }
+    } else {
+      return res
+        .status(200)
+        .send({ status: 200, statusText: "fail", message: "用户名不存在" });
+    }
   } catch (err) {
     next(err);
   }
